3fefad86d9c128f58cf89a3ea7d3f51cb76b9f49
zhoushengjie <zhou_shengjie@outlook.com>
2020-05-11 20:47:17 +0800

    build

--- modules/context.go
+++ modules/context.go
@@ -18,7 +18,7 @@ var ModuleContext moduleWrapper
 func Setup() {
 	//  redis
 	ModuleContext.Redis = redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
+		Addr:     "shunjiecloud-redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
