6521e23424c5975085a35bf3107dae7a197acbcd
zhoushengjie <zhou_shengjie@outlook.com>
2020-05-23 16:26:09 +0800

    build->deploy

--- build/build_bin.sh
+++
@@ -1,3 +0,0 @@
-projectDir=$(dirname "$PWD")
-echo $projectDir
-docker run --rm -v $projectDir:/projectDir -w /projectDir -e GOPROXY=https://mirrors.aliyun.com/goproxy,direct golang:1.14.2-buster go build -v -o /projectDir/build
---
+++ deploy/build_bin.sh
@@ -0,0 +1,3 @@
+projectDir=$(dirname "$PWD")
+echo $projectDir
+docker run --rm -v $projectDir:/projectDir -w /projectDir -e GOPROXY=https://mirrors.aliyun.com/goproxy,direct golang:1.14.2-buster go build -v -o /projectDir/deploy
